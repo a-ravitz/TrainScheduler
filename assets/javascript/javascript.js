ced8f1f199f832641a2e33de441885ece5e302a3
ravitz <adam.ravitz@gmail.com>
2019-03-25 14:56:44 -0400

    march 25.0

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,13 +6,16 @@ var config = {
     projectId: "class318-88119",
     storageBucket: "class318-88119.appspot.com",
     messagingSenderId: "262669921349"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-//event that happens when the add-train button is hit
-$("#add-train-btn").on("click", function(event) {
+
+var upToTheMiute;
+
+//add a train to the table at the top 
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
     //gathering the values from the variables for the form input fields 
@@ -20,7 +23,7 @@ $("#add-train-btn").on("click", function(event) {
     var tDestination = $("#destination-input").val().trim();
     var firstTime = $("#first-time-input").val().trim();
     var frequency = $("#frequency-input").val().trim()
-    
+
     //pushing the information to the database as key/value pairs
     database.ref().push({
         train: tName,
@@ -36,66 +39,105 @@ $("#add-train-btn").on("click", function(event) {
     $("#destionation-input").empty();
     $("#first-time-input").empty();
     $("#frequency-input").empty();
-
+    $("#train-table > tbody").empty();
+    childAdded()
 });
-$(document).on("click", ".remove", function() {
+
+//function to remove a row from the site and from firebase
+$(document).on("click", ".remove", function () {
     var tr = $(this).closest("tr");
     var key = tr.attr("data-key")
     console.log(key);
 
     database.ref(key).remove();
-    tr.remove(); 
+    tr.remove();
     console.log("deleted!");
+
+    $("#train-table > tbody").empty();
+    childAdded()
 });
 
-database.ref().on("child_added", function(Snapshot) {
-    console.log(Snapshot.key);
-  
-    // Store everything into a variable.
-    var tName = Snapshot.val().train;
-    var tDestination = Snapshot.val().destination;
-    var firstTime = Snapshot.val().firstTime;
-    var frequency = Snapshot.val().frequency;
-    var clearBtn = $("<button>").addClass("remove btn btn-primary").text("Clear");
+//function to update a row on the site and on firebase
+$(document).on("click", ".update", function () {
+    var tr = $(this).closest("tr");
+    var key = tr.attr("data-key")
 
-        // $(".remove").on("click", function() {
-        //     $(this).closest("tr").remove(); 
-        //     console.log("deleted!");
-        // });
+    var tName = $("#train-name-input").val().trim();
+    var tDestination = $("#destination-input").val().trim();
+    var firstTime = $("#first-time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim()
 
-    //converting the first time 
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    //collecting the current time 
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    //getting the difference between the current time and the first time
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
-  
-    // Minute Until Train
-    var minutesTill = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + minutesTill);
-    
-    // Next Train
-    var nextTrain = moment().add(minutesTill, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    var newInfo = $("<tr>").attr("data-key", Snapshot.key).append(
-        $("<td>").text(tName),
-        $("<td>").text(tDestination),
-        $("<td>").text(frequency),
-        $("<td>").text(nextTrain),
-        $("<td>").text(minutesTill),
-        $("<td>").html(clearBtn)
-      );
-
-      $("#train-table > tbody").append(newInfo);
-
+    database.ref(key).update({
+        train: tName,
+        destination: tDestination,
+        firstTime,
+        frequency,
     });
+
+    alert("Thank you for updating that train")
+    $("#train-table > tbody").empty();
+    childAdded();
+    console.log("updated")
+})
+
+//function for pushing data to the table and figuring out time reamining, etc...
+function childAdded() {
+    $("#train-table > tbody").empty();
+
+    database.ref().on("child_added", function (Snapshot) {
+        console.log(Snapshot.key);
+
+        // Store everything into a variable.
+        var tName = Snapshot.val().train;
+        var tDestination = Snapshot.val().destination;
+        var firstTime = Snapshot.val().firstTime;
+        var frequency = Snapshot.val().frequency;
+        var clearBtn = $("<button>").addClass("remove btn btn-primary").text("Clear");
+        var updateBtn = $("<button>").addClass("update btn btn-primary").text("Update");
+
+        //converting the first time 
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        //collecting the current time 
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        //getting the difference between the current time and the first time
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var minutesTill = frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + minutesTill);
+
+        // Next Train
+        var nextTrain = moment().add(minutesTill, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        var newInfo = $("<tr>").attr("data-key", Snapshot.key).append(
+            $("<td>").text(tName),
+            $("<td>").text(tDestination),
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrain),
+            $("<td>").text(minutesTill),
+            $("<td>").html(clearBtn),
+            $("<td>").html(updateBtn)
+        );
+
+        $("#train-table > tbody").append(newInfo);
+        update()
+    });
+};
+
+function update() {
+    
+    upToTheMiute = setInterval(childAdded, 60000);
+};
+
+
+childAdded()
